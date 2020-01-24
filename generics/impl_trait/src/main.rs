d7337079067ef29653b04de5cb8235fe72fb9754
itstunner <ahmedfawad928@gmail.com>
2020-01-24 21:41:54 +0500

    initial commit

--- generics/impl_trait/src/main.rs
+++ generics/impl_trait/src/main.rs
@@ -1,27 +1,25 @@
-use trai_library :: {Summary,Tweet, notify, returns_summarizable};
+use trai_library::{notify, returns_summarizable, Summary, Tweet};
 fn main() {
-
-let tweet = Tweet{
-    username: String :: from("horse_ebooks"),
-    content: String :: from("of course, as you probably already know, people"),
-    reply: false,
-    retweet: false,
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
     };
-        
+
     println!("1 new tweet: {}", tweet.summarize());
 
-    let tweet1 = Tweet{
-        username: String :: from("Guide book"),
-        content: String :: from("You are genius by the way"),
+    let tweet1 = Tweet {
+        username: String::from("Guide book"),
+        content: String::from("You are genius by the way"),
         reply: true,
         retweet: true,
     };
 
-    let name = String :: from("Fawad");
+    let name = String::from("Fawad");
 
     println!("retweet is: {:?}", notify(tweet1));
 
-let return_trait = returns_summarizable();
-println!("{}", &return_trait.username);
+    let return_trait = returns_summarizable();
+    println!("{}", &return_trait.username);
 }
-
